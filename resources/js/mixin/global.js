0bc182abcfcdb02a2f06c2cc8dfb303dd8cc39a2
Jason Xie <jason.xie@victheme.com>
2022-03-24 15:53:23 +0800

    removing unused function

--- resources/js/mixin/global.js
+++ resources/js/mixin/global.js
@@ -84,23 +84,6 @@ export const global = {
             }
     
             return decodeURIComponent(results[2].replace(/\+/g, ' '));
-        },
-        replaceUrlParam(url, paramName, paramValue) {
-            if (paramValue == null) {
-                paramValue = '';
-            }
-            let pattern = new RegExp('\\b(' + paramName + '=).*?(&|#|$)');
-            if (url.search(pattern) >= 0) {
-                return url.replace(pattern, '$1' + paramValue + '$2');
-            }
-            url = url.replace(/[?#]$/, '');
-            return url + (url.indexOf('?') > 0 ? '&' : '?') + paramName + '=' + paramValue;
-        },
-
-        changeLocale(value) {
-            if (value.locale) {
-                //window.location = this.replaceUrlParam(window.location.href, window.config.languageMenuSwitcher.params, value.locale);
-            }
         }
     }
 }
